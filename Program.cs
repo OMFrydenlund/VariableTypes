cd00a0a0e8c644caac36f6721223d73d23aad254
OMFrydenlund <migfrydenlund@gmail.com>
2023-11-15 12:58:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number = 7; // Integer
+        long severalNumbers = 1234;
+
+        double standardDecimal = 1.234;
+        float accurateDecimal = 1.234F;
+        decimal veryAccurateDecimal = 1.234M;
+
+        char letter = 'T';
+        string words = "T stands for terrible syntax";
+
+        bool butIsItReally = false;
+    }
+}
