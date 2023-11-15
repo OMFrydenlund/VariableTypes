c754b07d61a7cd5c558b37bb7f6bdce73c86c0df
OMFrydenlund <migfrydenlund@gmail.com>
2023-11-15 13:00:37 +0100

    Comments added

--- Program.cs
+++ Program.cs
@@ -5,13 +5,13 @@
         int number = 7; // Integer
         long severalNumbers = 1234;
 
-        double standardDecimal = 1.234;
-        float accurateDecimal = 1.234F;
-        decimal veryAccurateDecimal = 1.234M;
+        double standardDecimal = 1.234; //baseline
+        float accurateDecimal = 1.234F; //good
+        decimal veryAccurateDecimal = 1.234M; //expert
 
         char letter = 'T';
         string words = "T stands for terrible syntax";
 
-        bool butIsItReally = false;
+        bool butIsItReallyTerrible = false;
     }
 }
